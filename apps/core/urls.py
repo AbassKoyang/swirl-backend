e9a520f7952f57dc511014ba118f7ece57dab22c
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2026-01-05 13:17:49 +0100

    Initial commit

---
+++ apps/core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('', views.Home, name='home'),
+    path('auth/google_login/', views.google_login, name='google-login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
